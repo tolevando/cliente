c462847bda4414d54682601cbd6459483f2d1b81
Everton Almeida <everton@paynet.net.br>
2021-09-23 15:14:02 -0300

    alterações finais para mandar app para loja

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -9,7 +9,7 @@ import GoogleMaps
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?
   ) -> Bool {
-    GMSServices.provideAPIKey("SUA CHAVE AQUI")
+    GMSServices.provideAPIKey("AIzaSyCNpTXAVbIMet431sJNHpfai0v2-WgXSm0")
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
